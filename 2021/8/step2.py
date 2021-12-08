7781a993c730a7a5c5019df194bbaba46b254d88
Ricardo Ribalda <ribalda@chromium.org>
2021-12-08 09:39:13 +0100

    2021/08: cleaner

--- 2021/8/step2.py
+++ 2021/8/step2.py
@@ -1,64 +1,42 @@
 import sys
 import itertools
 
-seven_seg = [[1, 1, 1, 0, 1, 1, 1], [0, 0, 1, 0, 0, 1, 0], [1, 0, 1, 1, 1, 0, 1], [1, 0, 1, 1, 0, 1, 1], [0, 1, 1, 1, 0, 1, 0], [
-    1, 1, 0, 1, 0, 1, 1], [1, 1, 0, 1, 1, 1, 1], [1, 0, 1, 0, 0, 1, 0], [1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 0, 1, 1]]
-abcdefg = ["a", "b", "c", "d", "e", "f", "g"]
-
-
-def to_s(abc):
-    s = []
-    for i in range(len(abcdefg)):
-        if abcdefg[i] in abc:
-            s.append(1)
-        else:
-            s.append(0)
-    return s
+seven_seg = ["abcefg", "cf", "acdeg", "acdfg", "bcdf",
+             "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"]
+seven_seg_hash = {s: True for s in seven_seg}
 
 
 def remap(abc, perm):
-    out = [0] * 7
-    for i in range(len(abc)):
-        if abc[i]:
-            out[perm[i]] = 1
-    return out
+    abc = abc.translate(str.maketrans("abcdefg", perm))
+    return "".join(sorted(list(abc)))
 
 
-def find_map(ten):
-    for p in itertools.permutations(list(range(7))):
-        new_ten = []
+def find_permutation(ten):
+    for p in itertools.permutations(list("abcdefg")):
+        p = "".join(p)
         for t in ten:
-            new_ten.append(remap(t, p))
-
-        found = True
-        for n in new_ten:
-            if n not in seven_seg:
-                found = False
+            t = remap(t, p)
+            if t not in seven_seg_hash:
                 break
-
-        if found:
+        else:
             return p
     return None
 
 
-def convert_nums(nums):
-    outnum = ""
-    for e in nums:
-        e = remap(e, perm)
-        for i in range(len(seven_seg)):
-            if e == seven_seg[i]:
-                outnum += str(i)
-                continue
-    return int(outnum)
+def convert_nums(nums, perm):
+    outnum = 0
+    for n in nums:
+        outnum *= 10
+        n = remap(n, perm)
+        outnum += seven_seg.index(n)
+    return outnum
 
 
 lines = sys.stdin.readlines()
 sum_nums = 0
 for l in lines:
     ten, enabled = l.strip().split(" | ")
-    ten = list(map(to_s, ten.split()))
-    enabled = list(map(to_s, enabled.split()))
-    perm = find_map(ten)
-    sum_nums += convert_nums(enabled)
+    perm = find_permutation(ten.split())
+    sum_nums += convert_nums(enabled.split(), perm)
 
 print(sum_nums)
