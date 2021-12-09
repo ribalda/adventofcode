d0123e6ccf760d874ad4ec2d288d91dd21d386e4
Ricardo Ribalda <ricardo@ribalda.com>
2021-12-09 08:12:40 +0100

    2021/09: reuse step1

--- 2021/9/step2.py
+++ 2021/9/step2.py
@@ -6,20 +6,20 @@ def parse_elem(value):
     return [value, value >= 9]
 
 
-def get_min(mapa):
-    mpos = None
-    mval = None
+def get_basins(mapa):
+    basins = []
     for i in range(len(mapa)):
         for j in range(len(mapa[i])):
-            if mapa[i][j][1] == True:
-                continue
-            if not mpos:
-                mpos = [i, j]
-                mval = mapa[i][j][0]
-            if mapa[i][j][0] < mval:
-                mpos = [i, j]
-                mval = mapa[i][j][0]
-    return mpos
+            for p in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
+                n_i = i + p[0]
+                n_j = j + p[1]
+                if n_i < 0 or n_j < 0 or n_i >= len(mapa) or n_j >= len(mapa[i]):
+                    continue
+                if mapa[i][j][0] >= mapa[n_i][n_j][0]:
+                    break
+            else:
+                basins.append([i, j])
+    return basins
 
 
 def basin_len(mapa, min_pos):
@@ -50,17 +50,14 @@ for line in sys.stdin.readlines():
     line = list(map(parse_elem, list(line.strip())))
     mapa.append(line)
 
-basins = []
-while True:
-    min_pos = get_min(mapa)
-    if not min_pos:
-        break
-    b = basin_len(mapa, min_pos)
-    basins.append(b)
+basins = get_basins(mapa)
+basins_len = []
+for pos in basins:
+    b = basin_len(mapa, pos)
+    basins_len.append(b)
 
 mout = 1
-
-for m in sorted(basins)[-3:]:
+for m in sorted(basins_len)[-3:]:
     mout *= m
 
 print(mout)
