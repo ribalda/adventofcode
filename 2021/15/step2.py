896c39876c5cf59a313c90bfacc5c57cd60f3cce
Ricardo Ribalda <ricardo@ribalda.com>
2021-12-15 08:36:10 +0100

    2021/15: speedup

--- 2021/15/step2.py
+++ 2021/15/step2.py
@@ -1,33 +1,22 @@
 import sys
+import heapq
 
+def complex2int(origin):
+    return int(L_S*origin.real+origin.imag)
 
-def debug(L, mapa):
-    print("===")
-    for i in range(L):
-        out = ""
-        for j in range(L):
-            if complex(i, j) not in mapa:
-                out += "  *"
-            else:
-                out += format(mapa[complex(i, j)], '3d')
-        print(out)
-    print("")
-
-
-def pop_min(to_visit, distance):
-    min_idx = 0
-    for i, v in enumerate(to_visit):
-        if distance[v] < distance[to_visit[min_idx]]:
-            min_idx = i
-    return to_visit.pop(min_idx)
-
+def int2complex(n):
+    return complex(n//(L*S), n%(L*S))
 
 def get_distance(origin, end, mapa):
-    to_visit = [origin]
+    to_visit = [(0,complex2int(origin))]
     distance = {origin: 0}
 
     while to_visit:
-        n = pop_min(to_visit, distance)
+        #print(to_visit)
+        d,n = heapq.heappop(to_visit)
+        n = int2complex(n)
+        if d > distance[n]:
+            continue
         for i, j in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
             next_n = n + complex(i, j)
             if next_n not in mapa:
@@ -36,23 +25,24 @@ def get_distance(origin, end, mapa):
             if next_n in distance and distance[next_n] <= new_dist:
                 continue
             distance[next_n] = new_dist
-            if next_n != end:
-                to_visit += [next_n]
-            else:
+            if next_n == end:
                 return distance[end]
+            
+            heapq.heappush(to_visit, (distance[next_n], complex2int(next_n)))
 
     return distance[end]
 
 
 mapa = dict()
 lines = sys.stdin.readlines()
+L = len(lines)
+S = 5
+L_S = L * S
 for i, line in enumerate(lines):
     for j, l in enumerate(line.strip()):
-        for ii in range(5):
-            for jj in range(5):
-                v = int(l) + ii + jj
-                if v > 9:
-                    v -= 9
-                mapa[complex(i+ii*len(lines), j+jj*len(lines))] = v
+        for ii in range(S):
+            for jj in range(S):
+                v = (((int(l) + ii + jj) -1 ) % 9 ) + 1
+                mapa[complex(i+ii*L, j+jj*L)] = v
 
-print(get_distance(0j, complex(len(lines*5)-1, len(lines*5)-1), mapa))
+print(get_distance(0j, complex(S*L-1, S*L-1), mapa))
