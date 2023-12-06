15afafbcb2c49d9abb46b280c435881c2a695a71
Ricardo Ribalda <ricardo@ribalda.com>
2023-12-06 11:32:20 +0100

    2023/06: math

--- 2023/6/step1_2.py
+++ 2023/6/step1_2.py
@@ -2,13 +2,20 @@ import sys
 import math
 
 
-def calc_winning(t_d):
+def calc_winning_brute(t_d):
     out = 0
     t, d = t_d
     for s in range(1, t):
         if (t - s) * s > d:
             out += 1
     return out
+
+
+def calc_winning(t_d):
+    t, d = t_d
+    out1 = (t - math.sqrt(-4 * d + t * t - 4)) / 2
+    out2 = (math.sqrt(-4 * d + t * t - 4) + t) / 2
+    return math.floor(out2) - math.ceil(out1) + 1
 
 
 times, distances = sys.stdin.readlines()
