b258e54c34c6f888984d55360a18e8a7267c70c8
Ricardo Ribalda <ricardo@ribalda.com>
2024-12-05 09:40:11 +0100

    2024/05:simplify

--- 2024/5/step1_2.py
+++ 2024/5/step1_2.py
@@ -7,22 +7,14 @@ def orders2pagemin(orders):
     order = orders.splitlines()
     for order in order:
         a, b = order.split("|")
-        a, b = int(a), int(b)
-        pagemin.add((a, b))
+        pagemin.add((int(a), int(b)))
     return pagemin
 
 
-def valid_update(pagemin, update):
-    for i, a in enumerate(update):
-        for b in update[i + 1 :]:
-            if (b, a) in pagemin:
-                return False
-    return True
-
-def page_cmp(pagemin, x , y):
-    if (x,y) in pagemin:
+def page_cmp(pagemin, x, y):
+    if (x, y) in pagemin:
         return -1
-    if (y,x) in pagemin:
+    if (y, x) in pagemin:
         return 1
     return 0
 
@@ -31,19 +23,13 @@ orders, updates = sys.stdin.read().split("\n\n")
 
 pagemin = orders2pagemin(orders)
 
-out = 0
-unsorted = []
+out_1, out_2 = 0, 0
 for update in updates.splitlines():
     u = list(map(int, update.split(",")))
-    if valid_update(pagemin, u):
-        out += u[len(u) // 2]
+    sorted_u = sorted(u, key=cmp_to_key(lambda x, y: page_cmp(pagemin, x, y)))
+    if u == sorted_u:
+        out_1 += u[len(u) // 2]
     else:
-        unsorted.append(u) 
-
-print("Step 1:", out)
-
-out = 0
-for u in unsorted:
-    u = sorted(u, key = cmp_to_key(lambda x,y: page_cmp(pagemin, x, y)))
-    out += u[len(u) // 2]
-print("Step 2:", out)
+        out_2 += sorted_u[len(u) // 2]
+print("Step 1:", out_1)
+print("Step 2:", out_2)
