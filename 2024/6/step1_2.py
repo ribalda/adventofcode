1f0bb23f137590f199a5033ea6b5f6f067361006
Ricardo Ribalda <ricardo@ribalda.com>
2024-12-06 09:42:44 +0100

    2024/06: optimized
    Signed-off-by: Ricardo Ribalda <>

--- 2024/6/step1_2.py
+++ 2024/6/step1_2.py
@@ -1,18 +1,16 @@
 import sys
 
 
-def walk(world, pos):
-    visited = []
-    fast_v = set(visited)
+def walk(world, pos, visited):
+    visited = dict.fromkeys(visited)
     while True:
-        if pos in fast_v:
+        if pos in visited:
             return None
-        visited.append(pos)
-        fast_v.add(pos)
+        visited[pos] = None
         p, d = pos
         new_p = p + d
         if new_p not in world:
-            return visited
+            return list(visited)
         if world[new_p] == "#":
             pos = (p, d * (complex(0, -1)))
             continue
@@ -33,17 +31,22 @@ for x, line in enumerate(sys.stdin.readlines()):
         if val in to_dir:
             start_pos = (complex(x, y), to_dir[val])
 
-steps = walk(world, start_pos)
+steps = walk(world, start_pos, [])
 pos = {x[0] for x in steps}
 print("Step 1:", len(pos))
 
 
-n = 0
-for p in pos:
-    if p == start_pos[0]:
+tested_blockers = set()
+blockers = 0
+for i in range(1, len(steps)):
+    p, dir = steps[i]
+    if p in tested_blockers:
+        continue
+    if p == steps[i - 1][0]:
         continue
     world[p] = "#"
-    if not walk(world, start_pos):
-        n += 1
+    if not walk(world, steps[i - 1], steps[: i - 1]):
+        blockers += 1
     world[p] = "."
-print("Step 2:", n)
+    tested_blockers.add(p)
+print("Step 2:", blockers)
